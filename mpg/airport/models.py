adfb2d84dbc37954345d28cf8ca1f83d5eb4c36b
Natalia Tkachuk <NLToshevikova@gmail.com>
2024-06-04 00:22:43 +0300

    add Passenger model

--- mpg/airport/models.py
+++ mpg/airport/models.py
@@ -8,8 +8,14 @@ class Airport(models.Model):
     country = models.CharField(max_length=100)
     additional_info = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.code}, {self.name}, {self.city}, {self.country}"
+
 
 class Terminal(models.Model):
     airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.airport.name} - {self.name}"
