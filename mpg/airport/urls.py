5ad70b18f6d9c83ec3aae996a4b4f7cb170bd5c3
DashaJu <dashaju_u@mail.ru>
2024-06-24 21:21:45 +0300

    add dropdown airport search

--- mpg/airport/urls.py
+++ mpg/airport/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import AirportList, AirportDetail, AirportSearch
+from .views import AirportList, AirportDetail, AirportSearch, AirportSearchAutocomplete, search_airport
 
 
 urlpatterns = [
     path('', AirportList.as_view(), name='airport_list'),
     path('<int:pk>/', AirportDetail.as_view(), name='airport_detail'),
     path('search/', AirportSearch.as_view(), name='airport_search'),
+    path('airport_search_autocomplete/', AirportSearchAutocomplete.as_view(), name='airport_search_autocomplete'),
+    path('search_airport/', search_airport, name='searching'),
 ]
